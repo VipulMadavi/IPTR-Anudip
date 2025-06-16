d37d1fdfccbd8e295d1a375bc04779af672ec1aa
Vipul Shrawan Madavi <vipulmadavi1509@gmail.com>
2025-06-16 11:45:30 +0530

    up1

---
+++ Day8/abstraction.java
@@ -0,0 +1,23 @@
+public class abstraction {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.eat();
+    }
+
+}
+
+abstract class Animal {
+    abstract void sound();
+
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Barking");
+    }
+}
